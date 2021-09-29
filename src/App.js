bb1429c3ae16b26df5020c2b2d5a3df84442141b
Anthony Johnson <theanthonyjohnson@gmail.com>
2021-09-28 21:44:46 -0500

    basic scaffolding

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
-    <p>Hello World!</p>
+      <p>Hello World!</p>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return(
+      <>
+      <p>
+        Enter a city to search for it's location
+      </p>
+      </>
+    )
+  }
+}
+
+export default Main;
